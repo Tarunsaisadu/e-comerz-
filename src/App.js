866b067fbd784448f3f706d3e35b079b6ca7266d
Tarunsai Sadu <tarunsai.rct@gmail.com>
2022-06-09 11:08:23 +0200

    remove login

--- src/App.js
+++ src/App.js
@@ -16,62 +16,60 @@ import Sidebar from "./components/backoffice/Sidebar";
 import Userpage from "./components/backoffice/userpage/Userpage";
 import NewUser from "./components/backoffice/newuserpage/NewUser";
 function App() {
-  const adminUser = {
-    email: "abc@example.com",
-    password: "12345",
-  };
+  // const adminUser = {
+  //   email: "abc@example.com",
+  //   password: "12345",
+  // };
 
-  const [user, setUser] = useState({ name: "", email: "" });
+  // const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState(" ");
   const [enter, setEnter] = useState(false);
-  const Login = (details) => {
-    console.log(details);
-    if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
-    ) {
-      console.log("logged in");
-      setUser({
-        name: details.name,
-        email: details.email,
-      });
-    } else {
-      console.log("details do not match");
-      setError("details do not match");
-    }
-  };
+  // const Login = (details) => {
+  //   console.log(details);
+  //   if (
+  //     details.email == adminUser.email &&
+  //     details.password == adminUser.password
+  //   ) {
+  //     console.log("logged in");
+  //     setUser({
+  //       name: details.name,
+  //       email: details.email,
+  //     });
+  //   } else {
+  //     console.log("details do not match");
+  //     setError("details do not match");
+  //   }
+  // };
 
-  const Logout = () => {
-    console.log("Logout");
-    setUser({ name: "", email: "" });
-  };
+  // const Logout = () => {
+  //   console.log("Logout");
+  //   setUser({ name: "", email: "" });
+  // };
 
   return (
     <div className="App">
-      {user.email != "" ? (
-        <Router>
-          <Topnav Logout={Logout} />
+      {/* {user.email != "" ? ( */}
+      <Router>
+        <Topnav />
 
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(routerProps) => (
-                <Homepage {...routerProps} name={user.name} />
-              )}
-            />
-            <Route exact path="/Products" component={Products} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/purchase" component={Purchasemodal} />
-            <Route exact path="/backoffice" component={Backoffice} />
-            <Route exact path="/users" component={UserList} />
-            <Route exact path="/users/:userId" component={Userpage} />
-            <Route exact path="/newuser" component={NewUser} />
-          </Switch>
-        </Router>
-      ) : (
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(routerProps) => <Homepage {...routerProps} />}
+          />
+          <Route exact path="/Products" component={Products} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/purchase" component={Purchasemodal} />
+          <Route exact path="/backoffice" component={Backoffice} />
+          <Route exact path="/users" component={UserList} />
+          <Route exact path="/users/:userId" component={Userpage} />
+          <Route exact path="/newuser" component={NewUser} />
+        </Switch>
+      </Router>
+      {/* ) : (
         <Loginform Login={Login} error={error} />
-      )}
+      )} */}
     </div>
   );
 }
